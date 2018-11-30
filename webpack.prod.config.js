e31008ecf529f9da0fc04295bc6d4679942c60e4
Sam El-Husseini <samelh@microsoft.com>
2018-11-29 21:40:39 -0800

    Update prod build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,8 +1,10 @@
 
 const path = require('path'),
-    webpack = require('webpack')
+    webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'production',
     entry: {
         app: ["./src/index.tsx"],
     },
@@ -10,9 +12,6 @@ module.exports = {
         path: path.resolve(__dirname, 'docs'),
         filename: 'js/[name].bundle.js'
     },
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
@@ -29,17 +28,15 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: false,
+            react: 'production.min',
+            template: './src/index.html',
+            filename: 'index.html'
+        })
     ],
-    devServer: {
-        contentBase: [
-            path.join(__dirname, 'src'),
-            path.join(__dirname, 'docs'),
-            path.join(__dirname, 'node_modules'),
-        ],
-        hot: true,
-        port: 3000
-    },
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
